65cb1cea1d9e83f7c7e7cdbfc5b59b5a1eea4242
Matin <matin8288.gh@gmail.com>
2025-10-01 17:02:44 +0330

    creating input code

--- src/app/(Auth)/forgetPassword/step2/page.tsx
+++ src/app/(Auth)/forgetPassword/step2/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 import AuthForm from '@/components/Pages/AuthPages/AuthForm';
 import BackBtn from '@/components/Pages/AuthPages/BackBtn';
+import CodeInput from '@/components/Pages/AuthPages/CodeInput';
 import FormTitle from '@/components/Pages/AuthPages/FormTitle';
 import SubmitBtn from '@/components/Pages/AuthPages/SubmitBtn';
+import TimerCounter from '@/components/Pages/AuthPages/TimerCounter';
 import React from 'react'
 
 const ForgetPassPageStep2 = () => {
@@ -24,6 +26,14 @@ const ForgetPassPageStep2 = () => {
                 desc='کد تایید به ایمیل شما youremail@gmail.com ارسال می شود.'
             />
             {/* form title end */}
+
+            {/* digit code input */}
+            <CodeInput />
+            {/* digit code input end */}
+
+            {/* timer count down */}
+            <TimerCounter />
+            {/* timer count down end */}
 
             {/* submit btn */}
             <SubmitBtn title='ارسال' />
