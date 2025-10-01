8a027c9ae16b6ee8618641bafcb9e06c0786ed13
PoyaAzhkan <poya.azhkan@yahoo.com>
2025-10-02 00:24:20 +0330

    add specialsvila section to the landing page with bug fixed for base url and other thing...

--- src/components/Pages/Landing/Sections/SpecialVilas/index.tsx
+++ src/components/Pages/Landing/Sections/SpecialVilas/index.tsx
@@ -1,9 +1,26 @@
 import EmptyButton from "@/components/Ui/Buttons/EmptyButton";
 import React from "react";
+import Slider from "./Slider";
+import { GetSpecialVilas } from "@/core/Apis/GetSpecialVilas";
+import { IHouse } from "@/core/types/IHouse";
+import { AxiosResponse } from "axios";
 
-const SpecialVilas = () => {
+interface IApiResponse {
+  houses: IHouse[];
+  totalCount?: number;
+}
+
+const SpecialVilas = async () => {
+  // get houses Data 
+  const response = await GetSpecialVilas() as AxiosResponse<IApiResponse>
+  const { houses } = response.data;
+  
+  // filter all spacials house 
+  const filterData = houses.filter((items) => items.discounted_price !== null);
+  console.log('Data: ', filterData)
+
   return (
-    <div className="pb-30">
+    <div className="pb-30 space-y-8">
       {/* section topBar */}
       <div className="flex justify-between">
         <h3 className="text-[#1E2022] text-[24px] font-bold">
@@ -13,9 +30,7 @@ const SpecialVilas = () => {
       </div>
       {/* section topBar */}
       {/* the slider of specialVilas for rent */}
-      <div className="text-center text-red-600">
-         اسلایدر ویلا ها ...
-      </div>
+      <Slider filterData={filterData}/>
       {/* the slider of specialVilas for rent end */}
     </div>
   );
