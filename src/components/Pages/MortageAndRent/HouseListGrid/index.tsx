4f03580d4fa3083c99423cf1a7661cc29c3047cb
Matin <matin8288.gh@gmail.com>
2025-10-12 13:10:15 +0330

    get data

--- src/components/Pages/MortageAndRent/HouseListGrid/index.tsx
+++ src/components/Pages/MortageAndRent/HouseListGrid/index.tsx
@@ -1,40 +1,43 @@
 import HouseCard from '@/components/Ui/HouseCard';
-import { getHousesByFilter } from '@/core/Apis/GetHousesByFilter'
+import { getHousesByFilter } from '@/core/Apis/GetHousesByFilter';
 import { IApiResponse } from '@/core/Types/IApiResForGetHouses';
 import { AxiosResponse } from 'axios';
-import React, { useState } from 'react'
 
 const HouseListGrid = async () => {
-
-  const response = await getHousesByFilter() as AxiosResponse<IApiResponse>;
+  let response;
+  try {
+    response = await getHousesByFilter() as AxiosResponse<IApiResponse>;
+  } catch (error) {
+    console.error('خطا در دریافت داده‌ها:', error);
+    return <div className='text-center text-red-500'>خطایی در بارگذاری داده‌ها رخ داده است.</div>;
+  }
 
   const { houses } = response.data;
 
+  if (!houses || houses.length === 0) {
+    return <div className='text-center'>داده‌ای برای نمایش وجود ندارد.</div>;
+  }
+
   return (
     <div className='flex flex-col gap-10'>
-
-      {/* house list */}
-      <div className='bg-red-700'>
-        {
-          houses.map((item) => (
-            <HouseCard
-              HomeName={item.title}
-              HomeAddress={item.address}
-              HomePrice={item.price}
-              HomeOffer={item.discounted_price}
-              HomeImage={item.photos}
-              HomeBathroomCount={item.bathrooms}
-              HomeParkingCount={item.parking}
-              HomeCapacityCount={item.capacity}
-              HomeRoomCount={item.rooms}
-            />
-          ))
-        }
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {houses.map((item) => (
+          <HouseCard
+            key={item.id}
+            HomeName={item.title}
+            HomeAddress={item.address}
+            HomePrice={item.price}
+            HomeOffer={item.discounted_price}
+            HomeImage={item.photos}
+            HomeBathroomCount={item.bathrooms}
+            HomeParkingCount={item.parking}
+            HomeCapacityCount={item.capacity}
+            HomeRoomCount={item.rooms}
+          />
+        ))}
       </div>
-      {/* house list end */}
-
     </div>
-  )
-}
+  );
+};
 
-export default HouseListGrid
+export default HouseListGrid;
