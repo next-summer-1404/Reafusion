08249a926f66b9716081682b1ad1a9c0fb5f585f
PoyaAzhkan <poya.azhkan@yahoo.com>
2025-10-14 22:40:48 +0330

    bug fixed

--- src/app/(Main)/mortageAndRent/page.tsx
+++ src/app/(Main)/mortageAndRent/page.tsx
@@ -1,27 +1,31 @@
-'use client'
 import Breadcrumb from '@/components/Ui/Breadcrumb';
 import Container from '@/components/Ui/Container/Container';
-import React from 'react';
-import HouseListGrid from '@/components/Pages/MortageAndRent/HouseListGrid/index';
-import CustomSelectOption from '@/components/Ui/ReusableInputs/SelectOption';
-import CustomInputSearch from '../../../components/Ui/ReusableInputs/InputSearch/index';
+// import FilterBox from '@/components/Pages/MortageAndRent/FilterBox';
+import { GetAllHouses } from '@/core/Apis/GetAllHouses';
+import { AxiosResponse } from 'axios';
+import { IApiResponse } from '@/core/types/IApiResForGetHouses';
+import HouseCard from '@/components/Ui/HouseCard';
+import { FC } from 'react';
+import CustomPagination from '@/components/Ui/CustomPagination';
 
-const MortageAndRentPage = () => {
+interface IMortageAndRent {
+  searchParams: {
+    page?: string;
+  }
+}
+
+const MortageAndRentPage: FC<IMortageAndRent> = async ({ searchParams }) => {
+  const limit = 9;
+  const currentPage = parseInt(searchParams.page || "1", 10);  
+  const Search = '';
+  const transactionType = '';
+  const response = (await GetAllHouses(currentPage, limit, Search, transactionType)) as AxiosResponse<IApiResponse>;
+  const { houses, totalCount } = response.data;
+  
+  const totalPages = Math.ceil((totalCount as number) / limit);
+
   return (
     <Container className='flex flex-col gap-10'>
-
-      <div className='flex gap-5'>
-        <CustomInputSearch
-          labelText='جستجو'
-          placeholder='جستجو کنید ...'
-        />
-
-        <CustomSelectOption
-          labelText='مرتب سازی بر اساس'
-        />
-      </div>
-
-      {/* breadcrumb */}
       <Breadcrumb
         homeElement={'خانه'}
         activeClasses='!text-[#1E2022] !font-bold'
@@ -29,12 +33,25 @@ const MortageAndRentPage = () => {
         listClasses='hover:text-[#1E2022] text-[#777777]'
         capitalizeLinks
       />
-      {/* breadcrumb end */}
-
-      {/* house list grid */}
-      <HouseListGrid />
-      {/* house list grid end */}
-
+      {/* <FilterBox /> */}
+      <div className="pt-2 flex justify-between flex-wrap space-y-10 max-lg:justify-around">
+        {houses.map((house) => (
+          <HouseCard
+            key={house.id}
+            id={house.id}
+            HomeName={house.title}
+            HomeAddress={house.address}
+            HomePrice={house.price}
+            HomeOffer={house.discounted_price}
+            HomeImage={house.photos}
+            HomeBathroomCount={house.bathrooms}
+            HomeParkingCount={house.parking}
+            HomeCapacityCount={house.capacity}
+            HomeRoomCount={house.rooms}
+          />
+        ))}
+      </div>
+      <CustomPagination totalPages={totalPages} currentPage={currentPage} />
     </Container>
   );
 };
