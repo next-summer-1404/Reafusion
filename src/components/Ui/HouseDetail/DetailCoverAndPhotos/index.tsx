89b197579552d0471c5040a696f9ba0faefd41fa
Matin <matin8288.gh@gmail.com>
2025-10-23 17:38:14 +0330

    responsive detail page

--- src/components/Ui/HouseDetail/DetailCoverAndPhotos/index.tsx
+++ src/components/Ui/HouseDetail/DetailCoverAndPhotos/index.tsx
@@ -5,19 +5,19 @@ import HouseImage from "../../../../assets/images/HomeImgs/05.jpg";
 interface IProps {
   houseImages: string[];
   houseName: string;
-} 
+}
 
 const DetailCoverAndPhotos: FC<IProps> = ({ houseImages, houseName }) => {
   return (
-    <div className="h-[440px] flex justify-between">
+    <div className="h-[440px] flex justify-between max-md:flex-col max-md:h-fit max-md:gap-8">
       <Image
-        className="rounded-[24px]"
+        className="rounded-[24px] max-md:w-full"
         width={1000}
         height={440}
         src={houseImages === null ? HouseImage : (houseImages[0] || HouseImage)}
         alt={houseName}
       />
-      <div className="space-y-8">
+      <div className="space-y-8 flex max-md:flex-col">
         <Image
           className="rounded-[24px] h-[203px]"
           width={388}
