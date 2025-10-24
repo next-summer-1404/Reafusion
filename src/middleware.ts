5d50712f452393b40ea353b2d103b4b4f07c97a4
PoyaAzhkan <poya.azhkan@yahoo.com>
2025-10-24 14:59:53 +0330

    make better UX

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = async (request: NextRequest) => {
-  const token = request.cookies.get("my-token");
-  if (request.nextUrl.pathname.startsWith("/dashboard") && !token) {
+  const token = request.cookies.get("token");
+  const protectedPaths = ["/dashboard", "/FastReservePage"];
+  if ( protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path)) && !token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
   if (request.nextUrl.pathname.startsWith("/houses")) {
@@ -13,5 +14,5 @@ export const middleware = async (request: NextRequest) => {
 };
 
 export const config = {
-    matcher:["/dashboard/:path","/dashboard"]
+    matcher:["/dashboard/:path","/dashboard", '/FastReservePage/:path']
 }
