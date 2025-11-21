a9c61c82d80be92a332e0bfe99246ff373e9b7e5
Matin <matin8288.gh@gmail.com>
2025-11-21 13:22:10 +0330

    button cahnge

--- src/components/Pages/DashboardPages/DashboardHeader/HeaderMenu.tsx
+++ src/components/Pages/DashboardPages/DashboardHeader/HeaderMenu.tsx
@@ -1,10 +1,7 @@
-// components/HeaderMenu.tsx
 "use client";
-
 import React, { useState } from "react";
-import { Menu, X } from "lucide-react";
-import { IconButton } from "@mui/material";
 import DashboardMenu from "../DashboardMenu";
+import IconButton from "@/components/Ui/IconButton";
 
 const HeaderMenu = ({ role }: { role?: string }) => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -13,18 +10,10 @@ const HeaderMenu = ({ role }: { role?: string }) => {
     <>
       <div className="md:hidden">
         <IconButton
+          iconName="menu"
           onClick={() => setMobileMenuOpen(true)}
-          sx={{
-            width: 40,
-            height: 40,
-            bgcolor: "primary.main",
-            color: "white",
-            borderRadius: "50%",
-            "&:hover": { bgcolor: "primary.dark" },
-          }}
-        >
-          <Menu size={24} />
-        </IconButton>
+          customClass="!size-[40px] !p-0 items-center justify-center rounded-full bg-primary text-white !p-0"
+        />
       </div>
       {mobileMenuOpen && (
         <div className="fixed inset-0 z-50">
@@ -33,7 +22,7 @@ const HeaderMenu = ({ role }: { role?: string }) => {
             onClick={() => setMobileMenuOpen(false)}
           />
           <div className="fixed inset-y-0 right-0 w-[320px] bg-white shadow-2xl overflow-y-auto">
-              <DashboardMenu className='!w-full !max-md:block' role={role as string} />
+            <DashboardMenu className='!w-full !max-md:block' role={role as string} />
           </div>
         </div>
       )}
--- src/components/Ui/IconButton/index.tsx
+++ src/components/Ui/IconButton/index.tsx
@@ -1,10 +1,10 @@
-import { Building2, CircleCheck, ClockFading, Funnel, House, Plus, Sun, UserRoundCheck, UserRoundPen, UserRoundPlus } from 'lucide-react';
+import { Building2, CircleCheck, ClockFading, Funnel, House, Menu, Plus, Sun, UserRoundCheck, UserRoundPen, UserRoundPlus } from 'lucide-react';
 import React, { FC } from 'react'
 
 interface IProps {
     title?: string;
     customClass?: string;
-    iconName: 'building' | 'clock' | 'addUser' | 'checkedUser' | 'checked' | 'editUser' | 'house' | 'sun' | 'funnel' | 'plus';
+    iconName: 'building' | 'clock' | 'addUser' | 'checkedUser' | 'checked' | 'editUser' | 'house' | 'sun' | 'funnel' | 'plus' | 'menu';
     type?: 'submit';
     onClick?: () => void;
 }
@@ -20,6 +20,7 @@ const icons = [
     { name: 'sun', icon: <Sun size={24} strokeWidth={1.5} /> },
     { name: 'funnel', icon: <Funnel size={20} strokeWidth={1.5} /> },
     { name: 'plus', icon: <Plus size={24} strokeWidth={1.5} /> },
+    { name: 'menu', icon: <Menu size={24} strokeWidth={1.5} /> },
 ];
 
 const IconButton: FC<IProps> = ({ title, iconName, customClass, type, onClick }) => {
