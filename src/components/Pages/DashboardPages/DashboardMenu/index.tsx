1678c36dfd3ac90163ca09f45358ace1c725cab8
PoyaAzhkan <poya.azhkan@yahoo.com>
2025-11-24 21:17:27 +0330

    bug fixed

--- src/components/Pages/DashboardPages/DashboardMenu/index.tsx
+++ src/components/Pages/DashboardPages/DashboardMenu/index.tsx
@@ -1,142 +1,277 @@
-'use client'
-import Image from 'next/image';
-import React, { FC } from 'react';
-import logo from '@/assets/images/ReafusionLogo/MainReafusionLogo.jpg';
-import { Bell, Building, Building2, ChartBarDecreasing, ChartColumnStacked, CircleDollarSign, CreditCard, HandCoins, HeartPlus, LayoutDashboard, ListChecks, LogOut, MessageCircle, MessageCircleMore, ScrollText, UserRoundPen, Users } from 'lucide-react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+"use client";
+import Image from "next/image";
+import React, { FC } from "react";
+import logo from "@/assets/images/ReafusionLogo/MainReafusionLogo.jpg";
+import {
+  Bell,
+  Building,
+  Building2,
+  ChartBarDecreasing,
+  ChartColumnStacked,
+  CircleDollarSign,
+  CreditCard,
+  HandCoins,
+  HeartPlus,
+  LayoutDashboard,
+  ListChecks,
+  LogOut,
+  MessageCircle,
+  MessageCircleMore,
+  ScrollText,
+  UserRoundPen,
+  Users,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface IProps {
-    role: string;
-    className?: string;
+  role: string;
+  className?: string;
 }
 
 const DashboardMenu: FC<IProps> = ({ role, className }) => {
-    // get now url
-    const pathname = usePathname();
+  // get now url
+  const pathname = usePathname();
 
-    const hasManagementAccess = role === 'admin' || role === 'seller';
-    const isBuyerUser = role === 'admin' || role === 'buyer';
+  const hasManagementAccess = role === "admin" || role === "seller";
+  const isBuyerUser = role === "admin" || role === "buyer";
+  const idAdminUser = role === "admin";
 
-    // link list with icon
-    const menuItems = [
-        { href: '/dashboard', label: 'داشبورد', icon: <LayoutDashboard size={24} strokeWidth={1.5} /> },
-        { href: '/dashboard/profile', label: 'مشخصات کاربری', icon: <UserRoundPen size={24} strokeWidth={1.5} /> },
-        { href: '/dashboard/notifications', label: 'اعلان‌ها', icon: <Bell size={24} strokeWidth={1.5} /> },
-        ...(hasManagementAccess
-            ? [
-                { href: '/dashboard/placesManagement', label: 'مدیریت املاک', icon: <Building size={24} strokeWidth={1.5} /> },
-                { href: '/dashboard/financialManagement', label: 'مدیریت مالی', icon: <HandCoins size={24} strokeWidth={1.5} /> },
-            ]
-            : []
-        ),
-        { href: '/dashboard/reservesManagment', label: 'مدیریت رزروها', icon: <ListChecks size={24} strokeWidth={1.5} /> },
-        ...(isBuyerUser
-            ? [
-                { href: '/dashboard/paymentManagement', label: 'مدیریت پرداخت ها', icon: <CircleDollarSign size={24} strokeWidth={1.5} /> },
-                { href: '/dashboard/favoriteManagement', label: 'علاقه مندی ها', icon: <HeartPlus size={24} strokeWidth={1.5} /> },
-            ]
-            : []),
-        { href: '/dashboard/logout', label: 'خروج از حساب', icon: <LogOut size={24} strokeWidth={1.5} /> },
-        { href: '/dashboard/AllUsersManagement', label: 'مدیریت کاربران', icon: <Users size={24} strokeWidth={1.5} /> },
-        { href: '/dashboard/AllBookingManagement', label: 'مدیریت کل رزروها', icon: <ChartBarDecreasing size={24} strokeWidth={1.5} /> },
-        { href: '/dashboard/AllHousesManagement', label: 'مدیریت کل خانه ها', icon: <Building2 size={24} strokeWidth={1.5} /> },
-        { href: '/dashboard/CommentsManagement', label: 'مدیریت نظرات', icon: <MessageCircleMore size={24} strokeWidth={1.5} /> },
-        { href: '/dashboard/AllPaymentsManagments', label: 'مدیریت کل پرداخت ها', icon: <CreditCard size={24} strokeWidth={1.5} /> },
-        { href: '/dashboard/AllCategoriesManagement', label: 'مدیریت دسته بندی ها', icon: <ChartColumnStacked size={24} strokeWidth={1.5} /> },
-        { href: '/dashboard/AllDocumentsManagement', label: 'مدیریت سند ها', icon: <ScrollText size={24} strokeWidth={1.5} /> },
-    ];
-    // link list with icon end
+  // link list with icon
+  const menuItems = [
+    {
+      href: "/dashboard",
+      label: "داشبورد",
+      icon: <LayoutDashboard size={24} strokeWidth={1.5} />,
+    },
+    {
+      href: "/dashboard/profile",
+      label: "مشخصات کاربری",
+      icon: <UserRoundPen size={24} strokeWidth={1.5} />,
+    },
+    {
+      href: "/dashboard/notifications",
+      label: "اعلان‌ها",
+      icon: <Bell size={24} strokeWidth={1.5} />,
+    },
+    ...(hasManagementAccess
+      ? [
+          {
+            href: "/dashboard/placesManagement",
+            label: "مدیریت املاک",
+            icon: <Building size={24} strokeWidth={1.5} />,
+          },
+          {
+            href: "/dashboard/financialManagement",
+            label: "مدیریت مالی",
+            icon: <HandCoins size={24} strokeWidth={1.5} />,
+          },
+        ]
+      : []),
+    {
+      href: "/dashboard/reservesManagment",
+      label: "مدیریت رزروها",
+      icon: <ListChecks size={24} strokeWidth={1.5} />,
+    },
+    ...(isBuyerUser
+      ? [
+          {
+            href: "/dashboard/paymentManagement",
+            label: "مدیریت پرداخت ها",
+            icon: <CircleDollarSign size={24} strokeWidth={1.5} />,
+          },
+          {
+            href: "/dashboard/favoriteManagement",
+            label: "علاقه مندی ها",
+            icon: <HeartPlus size={24} strokeWidth={1.5} />,
+          },
+        ]
+      : []),
+    {
+      href: "/dashboard/logout",
+      label: "خروج از حساب",
+      icon: <LogOut size={24} strokeWidth={1.5} />,
+    },
+    ...(idAdminUser
+      ? [
+          {
+            href: "/dashboard/AllUsersManagement",
+            label: "مدیریت کاربران",
+            icon: <Users size={24} strokeWidth={1.5} />,
+          },
+          {
+            href: "/dashboard/AllBookingManagement",
+            label: "مدیریت کل رزروها",
+            icon: <ChartBarDecreasing size={24} strokeWidth={1.5} />,
+          },
+          {
+            href: "/dashboard/AllHousesManagement",
+            label: "مدیریت کل خانه ها",
+            icon: <Building2 size={24} strokeWidth={1.5} />,
+          },
+          {
+            href: "/dashboard/CommentsManagement",
+            label: "مدیریت نظرات",
+            icon: <MessageCircleMore size={24} strokeWidth={1.5} />,
+          },
+          {
+            href: "/dashboard/AllPaymentsManagments",
+            label: "مدیریت کل پرداخت ها",
+            icon: <CreditCard size={24} strokeWidth={1.5} />,
+          },
+          {
+            href: "/dashboard/AllCategoriesManagement",
+            label: "مدیریت دسته بندی ها",
+            icon: <ChartColumnStacked size={24} strokeWidth={1.5} />,
+          },
+          {
+            href: "/dashboard/AllDocumentsManagement",
+            label: "مدیریت سند ها",
+            icon: <ScrollText size={24} strokeWidth={1.5} />,
+          },
+        ]
+      : []),
+  ];
+  // link list with icon end
 
-    return (
-        <div className={`flex flex-col gap-8 py-8 bg-lightGray dark:bg-dark
+  return (
+    <div
+      className={`flex flex-col gap-8 py-8 bg-lightGray dark:bg-dark
          border border-borderColor dark:border-thidary rounded-[40px] max-lg:rounded-none lg:w-[25%] xl:w-[20%]
 
           ${className}
-          `}>
-            {/* logo */}
-            <div className="flex gap-4 items-center px-8 dark:text-whiteColor">
-                <Image className="rounded-full size-11" alt="" src={logo} width={32} height={32} />
-                <span className="text-[32px] font-bold">ریفیوژن</span>
-            </div>
-            {/* logo end */}
+          `}
+    >
+      {/* logo */}
+      <div className="flex gap-4 items-center px-8 dark:text-whiteColor">
+        <Image
+          className="rounded-full size-11"
+          alt=""
+          src={logo}
+          width={32}
+          height={32}
+        />
+        <span className="text-[32px] font-bold">ریفیوژن</span>
+      </div>
+      {/* logo end */}
 
-            {/* menu */}
-            <div className="flex flex-col px-8 max-xl:px-4 gap-8 overflow-y-auto">
-                {/* menu group */}
-                <div className="flex flex-col gap-4">
-                    <span className="text-sm text-primary dark:text-thidary">منو</span>
-                    {/* navigation */}
-                    <ul className="flex flex-col gap-6">
-                        {menuItems.slice(0, 3).map((item, index) => (
-                            <li key={index}>
-                                <Link
-                                    href={item.href}
-                                    className={`flex gap-4 items-center ${pathname === item.href
-                                        ? 'font-bold text-primary  dark:text-thidary'
-                                        : 'text-dark dark:text-whiteColor'
-                                        }`}
-                                >
-                                    <span className={pathname === item.href ? 'text-primary  dark:text-thidary' : 'text-dark dark:text-whiteColor'}>{item.icon}</span>
-                                    <span>{item.label}</span>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                    {/* navigation end */}
-                </div>
-                {/* menu group end */}
-
-                {/* group */}
-                <div className="flex flex-col gap-4">
-                    <span className="text-sm text-primary dark:text-thidary">مدیریت</span>
-                    {/* navigation */}
-                    <ul className="flex flex-col gap-6">
-                        {menuItems.slice(3, 9).map((item, index) => (
-                            <li key={index}>
-                                <Link
-                                    href={item.href}
-                                    className={`flex gap-4 items-center ${pathname === item.href
-                                        ? 'font-bold text-primary dark:text-thidary'
-                                        : 'text-dark dark:text-whiteColor'
-                                        }`}
-                                >
-                                    <span className={pathname === item.href ? 'text-primary dark:text-thidary' : item.label === 'خروج از حساب' ? 'text-red' : 'text-dark dark:text-whiteColor'}>{item.icon}</span>
-                                    <span className={pathname === item.href ? 'text-primary dark:text-thidary' : item.label === 'خروج از حساب' ? 'text-red' : 'text-dark dark:text-whiteColor'}>{item.label}</span>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                    {/* navigation end */}
-                </div>
-                {/* menu group end */}
-                {/* group */}
-                <div className="flex flex-col gap-4">
-                    <span className="text-sm text-primary dark:text-thidary">مدیریت سایت ( ادمین ) </span>
-                    {/* navigation */}
-                    <ul className="flex flex-col gap-6">
-                        {menuItems.slice(9).map((item, index) => (
-                            <li key={index}>
-                                <Link
-                                    href={item.href}
-                                    className={`flex gap-4 items-center ${pathname === item.href
-                                        ? 'font-bold text-primary dark:text-thidary'
-                                        : 'text-dark dark:text-whiteColor'
-                                        }`}
-                                >
-                                    <span className={pathname === item.href ? 'text-primary dark:text-thidary' : 'text-dark dark:text-whiteColor'}>{item.icon}</span>
-                                    <span>{item.label}</span>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                    {/* navigation end */}
-                </div>
-                {/* menu group end */}
-            </div>
-            {/* menu end */}
+      {/* menu */}
+      <div className="flex flex-col px-8 max-xl:px-4 gap-8 overflow-y-auto">
+        {/* menu group */}
+        <div className="flex flex-col gap-4">
+          <span className="text-sm text-primary dark:text-thidary">منو</span>
+          {/* navigation */}
+          <ul className="flex flex-col gap-6">
+            {menuItems.slice(0, 3).map((item, index) => (
+              <li key={index}>
+                <Link
+                  href={item.href}
+                  className={`flex gap-4 items-center ${
+                    pathname === item.href
+                      ? "font-bold text-primary  dark:text-thidary"
+                      : "text-dark dark:text-whiteColor"
+                  }`}
+                >
+                  <span
+                    className={
+                      pathname === item.href
+                        ? "text-primary  dark:text-thidary"
+                        : "text-dark dark:text-whiteColor"
+                    }
+                  >
+                    {item.icon}
+                  </span>
+                  <span>{item.label}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          {/* navigation end */}
         </div>
-    );
+        {/* menu group end */}
+
+        {/* group */}
+        <div className="flex flex-col gap-4">
+          <span className="text-sm text-primary dark:text-thidary">مدیریت</span>
+          {/* navigation */}
+          <ul className="flex flex-col gap-6">
+            {menuItems.slice(3, 9).map((item, index) => (
+              <li key={index}>
+                <Link
+                  href={item.href}
+                  className={`flex gap-4 items-center ${
+                    pathname === item.href
+                      ? "font-bold text-primary dark:text-thidary"
+                      : "text-dark dark:text-whiteColor"
+                  }`}
+                >
+                  <span
+                    className={
+                      pathname === item.href
+                        ? "text-primary dark:text-thidary"
+                        : item.label === "خروج از حساب"
+                        ? "text-red"
+                        : "text-dark dark:text-whiteColor"
+                    }
+                  >
+                    {item.icon}
+                  </span>
+                  <span
+                    className={
+                      pathname === item.href
+                        ? "text-primary dark:text-thidary"
+                        : item.label === "خروج از حساب"
+                        ? "text-red"
+                        : "text-dark dark:text-whiteColor"
+                    }
+                  >
+                    {item.label}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          {/* navigation end */}
+        </div>
+        {/* menu group end */}
+        {/* group */}
+        <div className="flex flex-col gap-4">
+          <span className="text-sm text-primary dark:text-thidary">
+            مدیریت سایت ( ادمین ){" "}
+          </span>
+          {/* navigation */}
+          <ul className="flex flex-col gap-6">
+            {menuItems.slice(9).map((item, index) => (
+              <li key={index}>
+                <Link
+                  href={item.href}
+                  className={`flex gap-4 items-center ${
+                    pathname === item.href
+                      ? "font-bold text-primary dark:text-thidary"
+                      : "text-dark dark:text-whiteColor"
+                  }`}
+                >
+                  <span
+                    className={
+                      pathname === item.href
+                        ? "text-primary dark:text-thidary"
+                        : "text-dark dark:text-whiteColor"
+                    }
+                  >
+                    {item.icon}
+                  </span>
+                  <span>{item.label}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          {/* navigation end */}
+        </div>
+        {/* menu group end */}
+      </div>
+      {/* menu end */}
+    </div>
+  );
 };
 
 export default DashboardMenu;
